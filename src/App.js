4dbe003aa2f608c34b7a937549e739be4713dd44
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-13 11:40:38 +0300

    passing props

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person'
-import Trial from './Trial/Trial'
 
 class App extends Component {
   render() {
@@ -11,9 +10,9 @@ class App extends Component {
         <h1>This is react boy!</h1>
         <p>This is really working</p>
 
-        <Person />
-        <hr/>
-        <Trial number= '2' />
+        <Person name='Humphrey' age='23'/>
+        <Person name='Alex' age='22' > Hobbies: riding </Person>
+        <Person name='Steph' age='20'/>
 
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const person = () => {
+const person = (props) => {
     return (
-        <p>Im a person</p>
+        <div>
+          <p>I'm {props.name} and i am {props.age} years old</p>
+           <p>{props.children}</p>
+        </div>
     )
 };
 export default person;
