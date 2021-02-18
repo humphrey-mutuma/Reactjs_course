649592f0ff14b10090c908792847982d488f3a55
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-18 12:18:59 +0300

    Class based props

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App appTitle='Person Manager'/>, 
     document.getElementById('root'));
 registerServiceWorker();
