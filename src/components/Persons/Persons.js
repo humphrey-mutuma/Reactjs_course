3d36908b6f6faf2c3d3047f939c7565bc08a1e55
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-20 11:02:50 +0300

    b4 PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -13,12 +13,17 @@ class Persons extends Component {
 
   shouldComponentUpdate(nextProps, nextState){
     console.log('[Persons.js] shouldComponentUpdate')
-    if(nextProps.persons !== this.props.persons){
+    if(
+      nextProps.persons !== this.props.persons ||
+      nextProps.changed !== this.props.changed ||
+      nextProps.clicked !== this.props.clicked
+      ){
       return true
     }else{
       return false
     }
   }
+
   getSnapshotBeforeUpdate(prevProps, prevState){
     console.log('[Persons.js]  getSnapshotBeforeUpdate');
     return null
