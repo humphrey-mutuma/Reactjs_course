4f3cabb0584d5e3409c17415d7e7a61d6d2cd7a2
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-20 10:29:41 +0300

    cleanup work with useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,48 +1,51 @@
-import React , { useEffect } from 'react';
-import classes from  "./Cockpit.css";
+import React, { useEffect } from 'react';
 
-const cockpit = ( props ) => {
+import classes from './Cockpit.css';
 
-    useEffect(() => {
-        console.log('[Cockpit.js] useEffect')
-        // HTTP request ...
-        setTimeout(() => {
-            alert('Saved data to the cloud!')
-        },1000);
-        return () => {
-            console.log('[Cockpit.js] cleanup work in useEffect')
-        };
-    }, []);
+const cockpit = props => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...
+    const timer setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+    return () => {
+        clearTimeout(timer);
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
 
-    useEffect(() => {
-        console.log('[Cockpit.js] 2nd useEffect')
-        return () => {
-            console.log('[Cockpit.js] cleanup work in 2nd useEffect')
-        };
-    })
-    const assignedClasses = [];
-    let btnClass = '';
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
 
-    if(props.showPerson) {
-        btnClass = classes.Red;
-    }
+  // useEffect();
 
-    if (props.persons.length <= 2) {
-      assignedClasses.push(classes.red); // classes = ['red']
-    }
-    if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
 
-    return(
-        <div className={classes.Cockpit}>
-            <h1>{props.title}</h1>
-            <p className={assignedClasses.join(' ')}>This is really working!</p>
-            <button className={btnClass} onClick={props.clicked}>
-                Toggle Persons
-            </button>
-        </div>
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red); // classes = ['red']
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
 
-    );
-}
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working!</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+      </button>
+    </div>
+  );
+};
+
 export default cockpit;
