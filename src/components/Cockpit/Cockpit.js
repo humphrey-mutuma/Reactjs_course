21b1f861be14a821b000dd609825c4398323ddb7
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-20 09:12:43 +0300

    useEffect hook

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React , { useEffect } from 'react';
 import classes from  "./Cockpit.css";
 
 const cockpit = ( props ) => {
+
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect')
+        // HTTP request ...
+    });
+
     const assignedClasses = [];
     let btnClass = '';
 
-    if(props.shoePerson) {
+    if(props.showPerson) {
         btnClass = classes.Red;
     }
 
