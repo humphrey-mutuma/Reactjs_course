9da4a9f1d7510549602d8dd730efda698906ad82
humphrey-mutuma <humphreymutuma01@gmail.com>
2021-02-20 09:58:07 +0300

    Cleaning up with lifeCyle Hooks and useEffects()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -8,9 +8,18 @@ const cockpit = ( props ) => {
         // HTTP request ...
         setTimeout(() => {
             alert('Saved data to the cloud!')
-        },1000)
+        },1000);
+        return () => {
+            console.log('[Cockpit.js] cleanup work in useEffect')
+        };
     }, []);
 
+    useEffect(() => {
+        console.log('[Cockpit.js] 2nd useEffect')
+        return () => {
+            console.log('[Cockpit.js] cleanup work in 2nd useEffect')
+        };
+    })
     const assignedClasses = [];
     let btnClass = '';
 
